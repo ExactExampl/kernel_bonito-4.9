1849acfa59d5e7fb94d95dfed1cd98605a77de24
ExactExampl <xmpl@dontsleep.work>
2025-10-11 22:05:59 +0300

    crypto: arm64/aes-ce-cipher: fix build with CFI

--- arch/arm64/crypto/aes-ce-glue.c
+++ arch/arm64/crypto/aes-ce-glue.c
@@ -47,21 +47,6 @@ static int num_rounds(struct crypto_aes_ctx *ctx)
 extern void aes_cipher_encrypt(struct crypto_tfm *tfm, u8 dst[], u8 const src[]);
 extern void aes_cipher_decrypt(struct crypto_tfm *tfm, u8 dst[], u8 const src[]);
 
-#ifdef CONFIG_CFI_CLANG
-static inline void __cfi_aes_cipher_encrypt(struct crypto_tfm *tfm, u8 dst[], u8 const src[])
-{
-	aes_cipher_encrypt(tfm, dst, src);
-}
-
-static inline void __cfi_aes_cipher_decrypt(struct crypto_tfm *tfm, u8 dst[], u8 const src[])
-{
-	aes_cipher_decrypt(tfm, dst, src);
-}
-
-#define aes_cipher_encrypt __cfi_aes_cipher_encrypt
-#define aes_cipher_decrypt __cfi_aes_cipher_decrypt
-#endif
-
 void aes_cipher_encrypt(struct crypto_tfm *tfm, u8 dst[], u8 const src[])
 {
 	struct crypto_aes_ctx *ctx = crypto_tfm_ctx(tfm);
